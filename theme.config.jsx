a89f71c50ad3902ecca96bcc6d055ea8cb26b93d
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.attlocal.net>
2023-08-06 18:25:26 -0500

    added GA tracking

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,27 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
     logo: <span>PUBG Resource</span>,
     project: {
       link: 'https://github.com/shuding/nextra'
-    }
-    // ...
+    },
+    head: (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="PUBG Resource" />
+        <meta property="og:description" content="Gaming resources for PUBG players" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM2LLSD1XB"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LM2LLSD1XB');
+          `}
+        </script>
+      </>
+    )
   }
