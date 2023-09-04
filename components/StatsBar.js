2b29715aa056d9b6d4e8c472abc084fb53b5b66a
Bryan Zawlocki <bryan.zawlocki@gmail.com>
2023-09-04 14:24:54 -0500

    calculated dps and rpm in statsbar component

--- components/StatsBar.js
+++ components/StatsBar.js
@@ -7,31 +7,26 @@ const StatsBar = ({ weaponName }) => {
 
   if (!weapon) return null;
 
-  const { damage, dps, rof, velocity, range, stability } = weapon;
-
+  const { damage, rof, velocity, range, stability } = weapon;
+  const dps = Math.round((1 / rof) * damage);
+  const rpm = Math.round((1 / rof) * 60);
 
   const rangeMax = 800;
   const rofMax = 3;
   const velMax = 1000;
-
+  
   /*EQUATIONS*/
-
   const invertedRof = (rof, max) => 100 - ((rof/max)*100);
   const standardEquation = (value, max) => (value / max) * 100;
-
-  const calculateROF = (timeBetweenShots) => 1 / timeBetweenShots;
-  const calculateDPS = (timeBetweenShots, damagePerShot) => {
-    const rateOfFire = 1 / timeBetweenShots;
-    return rateOfFire * damagePerShot;
-  };
 
   return (
     <div className={styles.statsContainer}>
       <h3>Stats</h3>
       {renderStat('Damage', damage, standardEquation)}
-      {renderStat('DPS', dps, standardEquation)}
-      {renderStat('ROF', rof, invertedRof, rofMax)}
-      {renderStat('Velocity', velocity, standardEquation, velMax)}
+      {renderStat('Damage Per Second', dps, standardEquation, 1000)}
+      {renderStat('Rate of Fire', rof, invertedRof, rofMax)}
+      {renderStat('Rounds Per Minute', rpm, standardEquation, 1500)}
+      {renderStat('Muzzle Velocity', velocity, standardEquation, velMax)}
       {renderStat('Range', range, standardEquation, rangeMax)}
       {renderStat('Stability', stability, standardEquation)}
     </div>
