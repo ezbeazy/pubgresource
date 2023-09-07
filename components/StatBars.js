af43479f36e5662b77b1b5e11cb2aa617498544b
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-07 15:35:48 -0500

    added damageMax variable for AP in StatBars

--- components/StatBars.js
+++ components/StatBars.js
@@ -8,7 +8,7 @@ const StatBars = ({ weaponName }) => {
 
   if (!weapon) return null;
 
-  const { damage, rof, velocity, range } = weapon;
+  const { type, damage, rof, velocity, range } = weapon;
   const dps = Math.round((1 / rof) * damage);
   const rpm = Math.round((1 / rof) * 60);
 
@@ -16,10 +16,16 @@ const StatBars = ({ weaponName }) => {
   const rofMax = 3;
   const invertRof = rofMax - rof;
 
+  let damageMax = 125;
+
+  if (type == "AP") {
+    damageMax = 300;
+  };
+
   return (
     <div className={styles.statsContainer}>
       <h3>Stats</h3>
-      <StatBar label="Damage" value={damage} max={125} />
+      <StatBar label="Damage" value={damage} max={damageMax} />
       <StatBar label="Damage Per Second" value={dps} max={1000} />
       <StatBar label="Rate of Fire" value={rof} fill={invertRof} max={3} />
       <StatBar label="Rounds Per Minute" value={rpm} max={1500} />
