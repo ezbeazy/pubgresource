1dc07152d534cf72b9680435801c10d70f74fee9
Bryan Zawlocki <bryan.zawlocki@gmail.com>
2023-09-13 15:31:19 -0500

    refactored weapon.js to no longer need StatBars component and deleted StatBars component

--- components/Weapon.js
+++ components/Weapon.js
@@ -1,13 +1,65 @@
 import styles from './Weapon.module.css';
 import weaponData from '../data/weapon.json';
-import StatBars from '../components/StatBars';
+import StatBar from './StatBar';
 
 const Weapon = ({ weaponName }) => {
 
   const weapon = weaponData.find(w => w.name === weaponName);
   if (!weapon) return null;
 
-  const { class: className, image, caliber, capacity, ironSiteZero, fireSelect } = weapon;
+  const { class: className, type, damage, rof, velocity, range, image, caliber, capacity, ironSiteZero, fireSelect } = weapon;
+
+  const dps = Math.round((1 / rof) * damage);
+  const rpm = Math.round((1 / rof) * 60);
+
+  let rofMax;
+  let damageMax;
+
+  if(type == "AP" || type == "MISC" || type == "SG"){
+    rofMax = 3;
+  } else {
+    rofMax = 2;
+  }
+
+  if(type == "AP") {
+    damageMax = 300;
+  } else {
+    damageMax = 125;
+  };
+
+  /*Invert ROF for StatBar fill*/
+  const invertRof = rofMax - rof;
+  
+  const statBars = () => {
+    if ( className == "Melee" ) {
+      return (
+        <div className={styles.statsContainer}>
+          <StatBar label="Damage" value={damage} max={damageMax} />
+          <StatBar label="Range" value={range} max={1000} />
+        </div>
+      );
+    } else if ( className == "Throwable" ) {
+      return (
+        <div className={styles.statsContainer}>
+          <StatBar label="Damage" value={damage} max={damageMax} />
+          <StatBar label="Damage Per Second" value={dps} max={1000} />
+          <StatBar label="Effective Radius" value={rof} fill={invertRof} max={3} />
+          <StatBar label="Range" value={range} max={1000} />
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.statsContainer}>
+          <StatBar label="Damage" value={damage} max={damageMax} />
+          <StatBar label="Damage Per Second" value={dps} max={1000} />
+          <StatBar label="Rate of Fire" value={rof} fill={invertRof} max={3} />
+          <StatBar label="Rounds Per Minute" value={rpm} max={1500} />
+          <StatBar label="Muzzle Velocity" value={velocity} max={1200} />
+          <StatBar label="Range" value={range} max={1000} />
+        </div>
+      );
+    };
+  }
 
   return (
     <div>
@@ -23,7 +75,8 @@ const Weapon = ({ weaponName }) => {
         <p><strong>Fire Select:</strong> {fireSelect}</p>
       </div>
     </div>
-    <StatBars weaponName={weaponName}/>
+    <h3>Stats</h3>
+    {statBars()}
     </div>
   );
 };
