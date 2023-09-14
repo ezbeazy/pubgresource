2af2b77dce9d54b7df10aeedb9d7c3f83cb571e8
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-14 13:00:35 -0500

    minor changes to melee component

--- components/Melee.js
+++ components/Melee.js
@@ -2,28 +2,33 @@ import styles from './Melee.module.css';
 import weaponData from '../data/weaponData';
 import StatBar from './StatBar';
 
-const Melee = ({ meleeName }) => {
+const MAX_RANGE = 100;
+const MAX_DAMAGE = 100;
 
-  const melee = weaponData.find(w => w.name === meleeName);
-  if (!melee) return null;
+const Melee = ({ meleeName }) => {
+  const meleeWeapon = weaponData.find(w => w.name === meleeName);
+
+  if (!meleeWeapon) {
+    return <div>Error: Melee weapon not found</div>;
+  }
   
-  const { class: weaponClass, type, image, link, meleeDamage, damage, range, space} = melee;
+  const { class: weaponClass, type, image, link, meleeDamage, damage, range, space} = meleeWeapon;
 
   return (
     <>
     <div className={styles.weaponContainer}>
       <div className={styles.imageContainer}>
-        <img src={image} alt="Weapon" className={styles.weaponImage} />
+        <img src={image} alt={`Melee Weapon: ${meleeName}`} className={styles.weaponImage} />
       </div>
       <aside className={styles.aside}>
         <div className={styles.infoContainer}>
           <p><span className={styles.label}>Class:</span> {weaponClass}</p>
           <p><span className={styles.label}>Type:</span> {type}</p>
         </div>
-        <div weaponClasse={styles.statsContainer}>
-          <StatBar label="Range" value={range} max={100} />
-          <StatBar label="Melee Damage" value={meleeDamage} max={100} />
-          <StatBar label="Throw Damage" value={damage} max={100} />
+        <div className={styles.statsContainer}>
+          <StatBar label="Range" value={range} max={MAX_RANGE} />
+          <StatBar label="Melee Damage" value={meleeDamage} max={MAX_DAMAGE} />
+          <StatBar label="Throw Damage" value={damage} max={MAX_DAMAGE} />
         </div>
       </aside>
     </div>
