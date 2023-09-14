045ef2667dd9aa76df82d91195d2223862035e0b
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-14 14:25:00 -0500

    finished throwawble component and page

--- components/Throwable.js
+++ components/Throwable.js
@@ -1,4 +1,4 @@
-import styles from './Weapon.module.css';
+import styles from './Throwable.module.css';
 import weaponData from '../data/weaponData';
 import StatBar from './StatBar';
 
@@ -7,33 +7,34 @@ const Throwable = ({ throwableName }) => {
   const throwable = weaponData.find(w => w.name === throwableName);
   if (!throwable) return null;
   
-  const { class: weaponClass, type, image, link, damage, rof, range, fuse, cook, effectDuration, radius, space} = throwable;
+  const { class: weaponClass, type, image, link, damage, dps, rof, range, fuse, cook, effectDuration, radius, space} = throwable;
 
-  const dps = Math.round((1 / rof) * damage);
-  const damageMax = 400;
+  //const dps = Math.round((1 / rof) * damage);
 
   return (
-    <div>
+    <>
     <div className={styles.weaponContainer}>
       <div className={styles.imageContainer}>
         <img src={image} alt="Weapon" className={styles.weaponImage} />
       </div>
-      <div className={styles.infoContainer}>
-        <p><strong>Class:</strong> {weaponClass}</p>
-        <p><strong>Type:</strong> {type}</p>
-        <p><strong>Weight:</strong> {space}</p>
-        <p><strong>Cook Time:</strong> {cook}</p>
-      </div>
+      <aside className={styles.aside}>
+        <div className={styles.infoContainer}>
+          <p><span className={styles.label}>Type:</span> {type}</p>
+          <p><span className={styles.label}>Item Weight:</span> {space}</p>
+          <p><span className={styles.label}>Cookable:</span> {cook}</p>
+        </div>
+        <div weaponClasse={styles.statsContainer}>
+      <StatBar label="Fuse (s)" value={fuse} max={16} />
+      <StatBar label="Max Damage" value={damage} max={225} />
+      <StatBar label="Damage Per Second" value={dps} max={225} />
+      <StatBar label="Effective Radius (m)" value={radius} max={100} />
+      <StatBar label="Effect Duration (s)" value={effectDuration} max={20} />
+      <StatBar label="Range (m)" value={range} max={100} />
     </div>
-    <h3>Stats</h3>
-      <div weaponClasse={styles.statsContainer}>
-        <StatBar label="Fuse" value={fuse} max={16} />
-        <StatBar label="Damage" value={damage} max={damageMax} />
-        <StatBar label="Damage Per Second" value={dps} max={1000} />
-        <StatBar label="Effective Radius" value={radius} max={100} />
-        <StatBar label="Range" value={range} max={1000} />
-      </div>
+      </aside>
     </div>
+    
+    </>
   );
 };
 
