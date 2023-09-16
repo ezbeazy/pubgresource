d02a938b7796a62b68cac90d028fb81553c9f76f
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-15 19:39:11 -0500

    refactored weapon.js included crate icon for crate weapons

--- components/Weapon.js
+++ components/Weapon.js
@@ -9,7 +9,7 @@ const Weapon = ({ name }) => {
     return <div>Error: Weapon not found</div>;
   }
   
-  const { class: weaponClass, type, image, link, caliber, capacity, ironSiteZero, fireSelect, damage, rof, velocity, range} = weapon;
+  const { class: weaponClass, type, image, link, caliber, capacity, ironSiteZero, fireSelect, crateWeapon, damage, rof, velocity, range} = weapon;
 
   const dps = Math.round((1 / rof) * damage);
   const rpm = Math.round((1 / rof) * 60);
@@ -28,34 +28,45 @@ const Weapon = ({ name }) => {
   } else {
     MAX_DAMAGE = 125;
   };
+
+  const crate = () => {
+    if(crateWeapon == true) {
+      return (
+        <div className={styles.crate}>
+          <img src="/img/icons/CarePackage_Normal.webp" alt="Crate Weapon"  />
+        </div>
+      );
+    };
+  };
  
   /*Invert ROF for StatBar fill*/
   const invertRof = MAX_ROF - rof;
 
   return (
-    <div>
+    <>
     <div className={styles.weaponContainer}>
-      <div className={styles.imageContainer}>
-        <img src={image} alt="Weapon" className={styles.weaponImage} />
+    {crate()}
+      <div className={styles.weaponProfile}>
+        <div className={styles.weaponImage}>
+          <img src={image} alt="Weapon"/>
+        </div>
+        <aside className={styles.infoContainer}>
+          <p><strong>Caliber:</strong> {caliber}</p>
+          <p><strong>Capacity:</strong> {capacity}</p>
+          <p><strong>Iron Site Zero:</strong> {ironSiteZero}</p>
+          <p><strong>Fire Select:</strong> {fireSelect}</p>
+        </aside>
+        </div>
       </div>
-      <aside className={styles.aside}>
-      <div className={styles.infoContainer}>
-        <p><strong>Caliber:</strong> {caliber}</p>
-        <p><strong>Capacity:</strong> {capacity}</p>
-        <p><strong>Iron Site Zero:</strong> {ironSiteZero}</p>
-        <p><strong>Fire Select:</strong> {fireSelect}</p>
-      </div>
-      </aside>
+      <div weaponClass={styles.statsContainer}>
+        <StatBar label="Damage" value={damage} max={MAX_DAMAGE} />
+        <StatBar label="Damage Per Second" value={dps} max={1000} />
+        <StatBar label="Rate of Fire" value={MAX_ROF} fill={invertRof} max={3} />
+        <StatBar label="Rounds Per Minute" value={rpm} max={1500} />
+        <StatBar label="Muzzle Velocity" value={velocity} max={1200} />
+        <StatBar label="Range" value={range} max={1000} />
     </div>
-    <div weaponClass={styles.statsContainer}>
-      <StatBar label="Damage" value={damage} max={MAX_DAMAGE} />
-      <StatBar label="Damage Per Second" value={dps} max={1000} />
-      <StatBar label="Rate of Fire" value={MAX_ROF} fill={invertRof} max={3} />
-      <StatBar label="Rounds Per Minute" value={rpm} max={1500} />
-      <StatBar label="Muzzle Velocity" value={velocity} max={1200} />
-      <StatBar label="Range" value={range} max={1000} />
-    </div>
-    </div>
+    </>
   );
 };
 
