ca5fa690053442127928cea30c73c6b65b9ddbd2
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-19 14:16:26 -0500

    stat tables component and moved analytics scripts out of head in config to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
 import './styles.css'
+import Script from 'next/script'
  
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+    <Component {...pageProps} />
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1013496822643101"
+     crossOrigin="anonymous" />
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LM2LLSD1XB" />
+    <Script id="google-analytics">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-LM2LLSD1XB');
+      `}
+    </Script>
+    </>
+  )
+};
--- theme.config.jsx
+++ theme.config.jsx
@@ -25,20 +25,6 @@ export default {
         <meta property="og:title" content="PUBG Resource" />
         <meta property="og:description" content="Gaming resources for PUBG players" />
         <link rel="icon" href="/img/favicon.png" />
-
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1013496822643101"
-     crossOrigin="anonymous"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM2LLSD1XB"></script>
-        <script>
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LM2LLSD1XB');
-          `}
-        </script>
-        
       </>
     ),
     feedback: {
