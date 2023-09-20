659bcbb23be7f3f011dfd5848aff831a52938ac0
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-20 10:08:55 -0500

    working on map component

--- components/Map.js
+++ components/Map.js
@@ -1,33 +1,108 @@
-import React, { useState } from 'react';
-import mapData from '../data/maps.json';
+import React, { useState, useEffect } from 'react';
+import maps from '../data/maps.json';
 import styles from './Map.module.css';
 
-const Map = ({ mapName }) => {
-  const [showSecretRoom, setShowSecretRoom] = useState(true);
-  const currentMap = mapData.find(map => map.mapName === mapName);
+const Map = ({ name }) => {
+  const [zoom, setZoom] = useState(1);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const mapData = maps.find((map) => map.name === name);
 
-  if (!currentMap) return <p>Map not found.</p>;
+  const handleZoomIn = () => setZoom((prevZoom) => Math.min(prevZoom + 0.1, 3));
+  const handleZoomOut = () => setZoom((prevZoom) => Math.max(prevZoom - 0.1, 1));
 
-  const toggleSecretRoom = () => {
-    setShowSecretRoom(!showSecretRoom);
+  const handleMouseDown = () => {
+    setIsDragging(true);
+    console.log(isDragging);
+    console.log('mouse down');
+    
   };
 
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    console.log(isDragging);
+    console.log('mouse up');
+    
+  };
+
+  const handleMouseMove = (e) => {
+    //console.log('mouse move');
+    if (isDragging) {
+      setPosition((prev) => {
+        let newX = prev.x + e.movementX;
+        let newY = prev.y + e.movementY;
+  
+        const viewport = document.getElementById('mapViewport');
+        const viewportWidth = viewport.clientWidth;
+        const viewportHeight = viewport.clientHeight;
+        
+        const mapContent = document.getElementById('mapContent');
+        const mapContentWidth = mapContent.clientWidth;
+        const mapContentHeight = mapContent.clientHeight;
+
+        const mapContentRect = mapContent.getBoundingClientRect();
+        
+        const scaledMapWidth = mapContentRect.width;
+        const scaledMapHeight = mapContentRect.height;
+  
+        const minX = viewportWidth - mapContentWidth;
+        const maxX = 0;
+  
+        const minY = viewportHeight - mapContentHeight;
+        const maxY = 0;
+  
+        // Keep within boundaries
+        newX = Math.max(minX, Math.min(maxX, newX));
+        newY = Math.max(minY, Math.min(maxY, newY));
+  
+        return { x: newX, y: newY };
+      });
+    }
+  };
+  
+  useEffect(() => {
+    const mapViewport = document.getElementById('mapViewport');
+    mapViewport.addEventListener('mousemove', handleMouseMove);
+    mapViewport.addEventListener('mouseup', handleMouseUp);
+    mapViewport.addEventListener('mousedown', handleMouseDown);
+
+    return () => {
+      mapViewport.removeEventListener('mousemove', handleMouseMove);
+      mapViewport.removeEventListener('mouseup', handleMouseUp);
+      mapViewport.removeEventListener('mousedown', handleMouseDown);
+    };
+  }, [isDragging]);
+
   return (
-    <div className={styles.mapContainer}>
-      <img src={currentMap.mapImg} alt={mapName} className={styles.mapImg} />
-      {currentMap.secretRoomImg && (
-        <button onClick={toggleSecretRoom} className={`${styles['toggleButton']} ${showSecretRoom ? styles['active'] : ''}`}>
-        <img src="/img/map-elements/pubg-secret-key-tilted.webp" alt="icon" />
-        <span className={styles.buttonText}>Secret Rooms</span>
-        </button>
-      )}
-      {showSecretRoom && currentMap.secretRoomImg && (
-        <img
-          src={currentMap.secretRoomImg}
-          alt={`${mapName} Secret Room`}
-          className={styles.secretRoomImg}
-        />
-      )}
+    <div className={styles.container}>
+      <div className={styles.buttons}>
+        <button className={styles.zoomButton} onClick={handleZoomIn}>+</button>
+        <button className={styles.zoomButton} onClick={handleZoomOut}>-</button>
+      </div>
+      <div id="mapViewport" className={styles.viewport}>
+        <div id="mapContent" className={styles.contents}
+          style={{
+            transform: `translate(${position.x}px, ${position.y}px) scale(${zoom})`,
+          }}
+        >
+          <img 
+            src={mapData.image} 
+            alt={`${name} Map`} 
+            className={styles.mapImage} 
+            draggable="false" 
+          />
+          {mapData.secretRooms.map((room, index) => (
+          <img
+            key={index}
+            src="/img/map-elements/pubg-secret-key-yellow.webp"
+            alt="Secret Room"
+            style={{ left: room.x, top: room.y }}
+            className={styles.roomIcon}
+            draggable="false"
+          />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
