00a39dafd4f3730bba503e6f59ac1f5c51b1f647
Bryan Zawlocki <bryan.zawlocki@gmail.com>
2023-09-21 19:09:45 -0500

    fixed map zoom boundaries and transitions

--- components/Map.js
+++ components/Map.js
@@ -6,22 +6,38 @@ const Map = ({ name }) => {
   const [zoom, setZoom] = useState(1);
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
+
   const mapData = maps.find((map) => map.name === name);
 
-  const handleZoomIn = () => setZoom((prevZoom) => Math.min(prevZoom + 0.1, 3));
-  const handleZoomOut = () => setZoom((prevZoom) => Math.max(prevZoom - 0.1, 1));
+  const handleZoomIn = () => setZoom((prevZoom) => Math.min(prevZoom + 1, 10));
+  const handleZoomOut = () => setZoom((prevZoom) => Math.max(prevZoom - 1, 1));
 
-  const handleMouseDown = () => {
-    setIsDragging(true);
-  };
+  const handleMouseDown = () => setIsDragging(true);
+  const handleMouseUp = () => setIsDragging(false);
+  const handleMouseLeave = () => setIsDragging(false);
 
-  const handleMouseUp = () => {
-    setIsDragging(false);
+  const adjustBoundaries = (newX, newY) => {
+    const viewportBox = document.getElementById('mapViewport').getBoundingClientRect();
+    const viewportWidth = viewportBox.width;
+    const viewportHeight = viewportBox.height;
+  
+    const contentBox = document.getElementById('mapContent').getBoundingClientRect();
+    const contentWidth = contentBox.width;
+    const contentHeight = contentBox.height;
+  
+    const minX = (viewportWidth - contentWidth) / 2;
+    const maxX = (contentWidth - viewportWidth) / 2;
+  
+    const minY = (viewportHeight - contentHeight) / 2;
+    const maxY = (contentHeight - viewportHeight) / 2;
+  
+    // Keep within boundaries
+    newX = Math.max(minX, Math.min(maxX, newX));
+    newY = Math.max(minY, Math.min(maxY, newY));
+  
+    return { x: newX, y: newY };
   };
-
-  const handleMouseLeave = () => {
-    setIsDragging(false);
-  };
+  
 
   const handleMouseMove = (e) => {
 
@@ -30,25 +46,8 @@ const Map = ({ name }) => {
         let newX = prev.x + e.movementX;
         let newY = prev.y + e.movementY;
   
-        const viewportBox = document.getElementById('mapViewport').getBoundingClientRect();
-        const viewportWidth = viewportBox.width;
-        const viewportHeight = viewportBox.height;
-        
-        const contentBox = document.getElementById('mapContent').getBoundingClientRect();
-        const contentWidth = contentBox.width;
-        const contentHeight = contentBox.height;
-  
-        const minX = (viewportWidth - contentWidth)/2;
-        const maxX = (contentWidth - viewportWidth)/2;
-  
-        const minY = (viewportHeight - contentHeight)/2;
-        const maxY = (contentHeight - viewportHeight)/2;
-  
-        // Keep within boundaries
-        newX = Math.max(minX, Math.min(maxX, newX));
-        newY = Math.max(minY, Math.min(maxY, newY));
-  
-        return { x: newX, y: newY };
+        const adjusted = adjustBoundaries(newX, newY);
+        return adjusted;
       });
     }
   };
@@ -67,6 +66,50 @@ const Map = ({ name }) => {
       mapViewport.removeEventListener('mouseleave', handleMouseLeave);
     };
   }, [isDragging]);
+
+  //translate map position
+  useEffect(() => {
+    const mapContent = document.getElementById('mapContent');
+    mapContent.style.transform = `translate(${position.x}px, ${position.y}px) scale(${zoom})`;
+    mapContent.style.transition = 'transform 100ms ease';
+  }, [position.x, position.y]);
+
+  //translate map scale
+  useEffect(() => {
+    const mapContent = document.getElementById('mapContent');
+    mapContent.style.transform = `scale(${zoom})`;
+    mapContent.style.transition = 'transform 1s ease';
+
+    if (zoom <= 1) {
+      setPosition({ x: 0, y: 0 }); // Reset position
+    }
+
+    const adjusted = adjustBoundaries(position.x, position.y);
+    setPosition(adjusted);
+  }, [zoom]);
+
+  useEffect(() => {
+    setPosition((prev) => {
+      const viewportBox = document.getElementById('mapViewport').getBoundingClientRect();
+      const viewportWidth = viewportBox.width;
+      const viewportHeight = viewportBox.height;
+  
+      const contentBox = document.getElementById('mapContent').getBoundingClientRect();
+      const contentWidth = contentBox.width * zoom;  // taking zoom into consideration
+      const contentHeight = contentBox.height * zoom;  // taking zoom into consideration
+  
+      const minX = (viewportWidth - contentWidth) / 2;
+      const maxX = (contentWidth - viewportWidth) / 2;
+  
+      const minY = (viewportHeight - contentHeight) / 2;
+      const maxY = (contentHeight - viewportHeight) / 2;
+  
+      let newX = Math.max(minX, Math.min(maxX, prev.x));
+      let newY = Math.max(minY, Math.min(maxY, prev.y));
+  
+      return { x: newX, y: newY };
+    });
+  }, [zoom]);
   
 
   return (
@@ -76,12 +119,7 @@ const Map = ({ name }) => {
         <button className={styles.zoomButton} onClick={handleZoomOut}>-</button>
       </div>
       <div id="mapViewport" className={styles.viewport}>
-        <div id="mapContent" className={styles.contents}
-          style={{
-            transform: `translate(${position.x}px, ${position.y}px) scale(${zoom})`,
-            transition: 'transform 0.1s ease'
-          }}
-        >
+        <div id="mapContent" className={styles.contents}>
           <img 
             src={mapData.image} 
             alt={`${name} Map`} 
