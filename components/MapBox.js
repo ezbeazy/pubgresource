6e3b4cb8913caf2286e72a9d04b7bdf2917ee75b
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-09-30 15:49:25 -0500

    reworked map component

--- components/MapBox.js
+++ components/MapBox.js
@@ -3,23 +3,34 @@ import maps from '../data/maps.json';
 import styles from './MapBox.module.css';
 
 const MapBox = ({ name }) => {
-  const [viewportSize, setViewportSize] = useState({ width: 0, height: 0 });
-  const [mapSize, setMapSize] = useState({ width: 0, height: 0 });
+  const viewportSizeRef = useRef({ width: 0, height: 0 });
+  const mapSizeRef = useRef({ width: 0, height: 0 });
+  const scaleRef = useRef(0);
+  const boundariesRef = useRef({ minX: 0, maxX: 0, minY: 0, maxY: 0 });
+  const isDraggingRef = useRef(false);
+  const touchStartRef = useRef({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
-  const [scale, setScale] = useState(1);
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [boundaries, setBoundaries] = useState({ minX: 0, maxX: 0, minY: 0, maxY: 0 });
-  const [isDragging, setIsDragging] = useState(false);
-  const [touchStart, setTouchStart] = useState({ x: 0, y: 0 });
 
   const mapData = maps.find((map) => map.name === name);
-
   const viewportRef = useRef(null);
   const layersRef = useRef(null);
 
+  const consoleLog = (location) => {
+    console.log(" ** NEW LOG ** "+location);
+    console.log('viewportSize: ', Object.values(viewportSizeRef.current));
+    console.log('viewport width: ', viewportSizeRef.current.width);
+    console.log('mapSize: ', Object.values(mapSizeRef.current));
+    console.log('map width: ', mapSizeRef.current.width);
+    console.log('zoom: ', zoom);
+    console.log('scale: ', scaleRef.current);
+    console.log('position: ', Object.values(position));
+    console.log('boundaries: ', Object.values(boundariesRef.current));
+  }
+
   const clampPosition = (x, y) => {
-    const clampedX = Math.min(Math.max(x, boundaries.minX), boundaries.maxX);
-    const clampedY = Math.min(Math.max(y, boundaries.minY), boundaries.maxY);
+    const clampedX = Math.min(Math.max(x, boundariesRef.current.minX), boundariesRef.current.maxX);
+    const clampedY = Math.min(Math.max(y, boundariesRef.current.minY), boundariesRef.current.maxY);
     return { x: clampedX, y: clampedY };
   };
   
@@ -43,40 +54,38 @@ const MapBox = ({ name }) => {
   const debouncedZoomOut = debounce(handleZoomOut, 50);
 
   const handleMouseDown = (e) =>  {
-    setIsDragging(true);
     if (e.type === 'touchstart') {
-      e.preventDefault(); // Prevent mouse event emulation
-      setTouchStart({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+      e.preventDefault(); 
+      touchStartRef.current = { x: e.touches[0].clientX, y: e.touches[0].clientY };
     }
+    isDraggingRef.current = true;
+    handleLiseners();
   };
   const handleMouseUp = (e) => {
-    setIsDragging(false);
     if (e.type === 'touchend') {
-      e.preventDefault(); // Prevent mouse event emulation
+      e.preventDefault(); 
     }
+    isDraggingRef.current = false;
+    handleLiseners();
   };
 
   const handleMouseMove = (e) => {
-    if (isDragging) {
+    if (isDraggingRef.current) {
       let movementX, movementY;
-  
       if (e.type === 'touchmove') {
-        e.preventDefault(); // Prevent mouse event emulation
+        e.preventDefault();
         const currentX = e.touches[0].clientX;
         const currentY = e.touches[0].clientY;
-        movementX = currentX - touchStart.x;
-        movementY = currentY - touchStart.y;
-        setTouchStart({ x: currentX, y: currentY });
+        movementX = currentX - touchStartRef.current.x;
+        movementY = currentY - touchStartRef.current.y;
+        touchStartRef.current = { x: currentX, y: currentY };
       } else {
         movementX = e.movementX;
         movementY = e.movementY;
       }
-  
       setPosition((prev) => {
         let newX = prev.x + movementX;
         let newY = prev.y + movementY;
-  
-        // Calculate the clamped position based on boundaries
         const clampedPosition = clampPosition(newX, newY);
         return clampedPosition;
       });
@@ -84,29 +93,30 @@ const MapBox = ({ name }) => {
   };
 
   const handleMouseLeave = (e) => {
-    setIsDragging(false);
+    isDraggingRef.current = false;
     if (e.type === 'touchcancel') {
       e.preventDefault(); // Prevent mouse event emulation
     }
+    handleLiseners();
+  };
+
+  const hciEvents = {
+    'mousedown': handleMouseDown,
+    'mouseup': handleMouseUp,
+    'mousemove': handleMouseMove,
+    'mouseleave': handleMouseLeave,
+    'touchstart': handleMouseDown,
+    'touchend': handleMouseUp,
+    'touchmove': handleMouseMove,
+    'touchcancel': handleMouseLeave
   };
 
   //Handle Listeners
-  useEffect(() => {
+  const handleLiseners = () => {
     const viewport = document.getElementById('viewport');
-   
-    const events = {
-      'mousedown': handleMouseDown,
-      'mouseup': handleMouseUp,
-      'mousemove': handleMouseMove,
-      'mouseleave': handleMouseLeave,
-      'touchstart': handleMouseDown,
-      'touchend': handleMouseUp,
-      'touchmove': handleMouseMove,
-      'touchcancel': handleMouseLeave
-    };
     
-    if (!isDragging) {
-      for (const [event, handler] of Object.entries(events)) {
+    if (!isDraggingRef.current) {
+      for (const [event, handler] of Object.entries(hciEvents)) {
         if (event === 'mousedown' || event === 'touchstart') {
           viewport.addEventListener(event, handler);
         } else {
@@ -114,7 +124,7 @@ const MapBox = ({ name }) => {
         }
       }
     } else {
-      for (const [event, handler] of Object.entries(events)) {
+      for (const [event, handler] of Object.entries(hciEvents)) {
         if (event !== 'mousedown' && event !== 'touchstart') {
           viewport.addEventListener(event, handler);
         } else {
@@ -124,76 +134,78 @@ const MapBox = ({ name }) => {
     }
 
     return () => {
-      for (const [event, handler] of Object.entries(events)) {
+      for (const [event, handler] of Object.entries(hciEvents)) {
         viewport.removeEventListener(event, handler);
       }
     };
-  }, [isDragging]);
+  };
 
+  const updateBoundaries = () => {
+    boundariesRef.current = {
+      minX: (viewportSizeRef.current.width - (mapSizeRef.current.width)) / 2,
+      maxX: ((mapSizeRef.current.width) - viewportSizeRef.current.width) / 2,
+      minY: (viewportSizeRef.current.height - (mapSizeRef.current.height)) / 2,
+      maxY: ((mapSizeRef.current.height) - viewportSizeRef.current.height) / 2,
+    };
+  };
 
   //Handle Dragging
   useEffect(() => {
-    const layers = document.getElementById('layers');
+    const layers = layersRef.current;
     layers.style.transform = `translate(${position.x}px, ${position.y}px) scale(${zoom})`;
     layers.style.transition = 'transform 50ms ease';
   }, [position]);
 
+  //Handle Zooming
   useEffect(() => {
-    const layers = document.getElementById('layers');
+    const layers = layersRef.current;
     const clampedPosition = clampPosition(position.x, position.y);
-
     layers.style.transform = `translate(${clampedPosition.x}px, ${clampedPosition.y}px) scale(${zoom})`;
     layers.style.transition = 'transform 1000ms ease';
 
-  }, [zoom, boundaries]);
+    updateBoundaries();
+  }, [zoom]);
 
   //Handle Resize Scaling
   useEffect(() => {
     
-    if(!viewportRef.current) return;
+    // Initialize only if the refs are available
+    if(!viewportRef.current || !layersRef.current) return;
 
+    // Handling resizing for viewport and layers
     const viewportObserver = new ResizeObserver(() => { 
-      if(viewportRef.current.offsetWidth !== viewportSize.width) {
-        setViewportSize({
+        viewportSizeRef.current = {
           width: viewportRef.current.offsetWidth,
           height: viewportRef.current.offsetHeight
-        });
-        setMapSize({
-          width: layersRef.current.offsetWidth,
-          height: layersRef.current.offsetHeight
-        });
-        setScale(
-          layersRef.current.offsetWidth / mapData.size.width
-        );
-      };
+        };
+        mapSizeRef.current = {
+          width: (layersRef.current.offsetWidth * zoom),
+          height: (layersRef.current.offsetHeight * zoom)
+        };
+        scaleRef.current = layersRef.current.offsetWidth / mapData.size.width;
+        updateBoundaries();
+        consoleLog('viewportObserver');
     });
 
     viewportObserver.observe(viewportRef.current);
 
-    return () => viewportObserver.disconnect();
-    
-  }, [viewportRef]);
-  
-  // Set Boundaries
-  useEffect(() => {
-    setBoundaries(() => {
-      const minX = (viewportSize.width - (mapSize.width * zoom)) / 2;
-      const maxX = ((mapSize.width * zoom) - viewportSize.width) / 2;
-      const minY = (viewportSize.height - (mapSize.height * zoom)) / 2;
-      const maxY = ((mapSize.height * zoom) - viewportSize.height) / 2;
-      return { minX, maxX, minY, maxY };
-    });
-  }, [viewportSize, zoom]);
+    // Handle Event Listeners on Mount and Unmount
+    const viewport = document.getElementById('viewport');
 
-  const consoleLog = () => {
-    console.log('viewportSize: ', Object.values(viewportSize));
-    console.log('mapSize: ', Object.values(mapSize));
-    console.log('zoom: ', zoom);
-    console.log('scale: ', scale);
-    console.log('position: ', Object.values(position));
-    console.log('boundaries: ', Object.values(boundaries));
-  }
-  consoleLog();
+    // Initially add mousedown event
+    viewport.addEventListener('mousedown', handleMouseDown);
+
+    return () => {
+      viewportObserver.disconnect(); 
+
+      for (const [event, handler] of Object.entries(hciEvents)) {
+        viewport.removeEventListener(event, handler);
+      }
+    }
+    
+  }, []);
+  
+  consoleLog('before return');
 
   return (
     <>
@@ -208,9 +220,11 @@ const MapBox = ({ name }) => {
             <img src={mapData.image} alt={`${name}`} draggable="false"/>
           </div>
           <div id="pins" className={styles.pins}>
-            {
-              mapData.secretRooms && mapData.secretRooms.map((room, index) => (
-                <div key={index} className={styles.pin} style={{left: (room.x * scale), top: (room.y * scale)}}>
+            { mapData.secretRooms && mapData.secretRooms.map((room, index) => (
+                <div 
+                  key={index} 
+                  className={styles.pin} 
+                  style={{left: (room.x * scaleRef.current), top: (room.y * scaleRef.current)}}>
                 </div>
               ))
             }
