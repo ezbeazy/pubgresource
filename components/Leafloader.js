a80defaec07691e9834e665350e80234c03e7fb0
Bryan Zawlocki <bryan.zawlocki@gmail.com>
2023-10-09 12:48:39 -0500

    got leaflet working

--- components/Leafloader.js
+++ components/Leafloader.js
@@ -1,15 +1,18 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 const LeafLoader = ({ name }) => {
 
+  const bounds = [[0, 0], [-256, 256]];
+
   return(
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
+    <MapContainer crs={L.CRS.Simple} maxBounds={bounds} maxBoundsViscosity={0} center={[256, 256]} zoom={1} maxZoom={5} minZoom={1} scrollWheelZoom={true} style={{ width: "100%", paddingBottom: "100%"}}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="http://localhost:3000/img/maps/leaflet/taego/{z}_{x}_{y}.jpg"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[-20, 20]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
