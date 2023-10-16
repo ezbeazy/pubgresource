7d0f3e9f33ca1e72195c76438c9662301660eefe
Bryan Zawlocki <bryanzawlocki@Bryans-iMac-2.local>
2023-10-16 15:41:36 -0500

    saved leaflet images at higher res

--- components/Leafloader.js
+++ components/Leafloader.js
@@ -17,11 +17,11 @@ const LeafLoader = ({ mapName, markers = [], polylines = [] }) => {
 });
 
   return(
-    <MapContainer crs={L.CRS.Simple} maxBounds={bounds} maxBoundsViscosity={1} center={[-128, 128]} zoom={1} maxZoom={5} minZoom={.5} scrollWheelZoom={true} attributionControl={false} style={{ width: "100%", paddingBottom: "100%"}}>
+    <MapContainer crs={L.CRS.Simple} maxBounds={bounds} maxBoundsViscosity={1} center={[-128, 128]} zoom={2} maxZoom={5} minZoom={1} scrollWheelZoom={true} attributionControl={false} style={{ width: "100%", paddingBottom: "100%"}}>
       <TileLayer
         tileSize={256}
         attribution='&copy; <a href="https://pubgresource.com">PubgResource</a> contributors'
-        url={`/img/maps/${mapName}/leaflet//{z}_{x}_{y}.webp`}
+        url={`/img/maps/${mapName}/leaflet/{z}_{x}_{y}.webp`}
       />
       {polylines.map((polyline, index) => (
         <Polyline key={index} positions={polyline.positions} color={polyline.color} weight={polyline.weight} opacity={polyline.opacity}/>
